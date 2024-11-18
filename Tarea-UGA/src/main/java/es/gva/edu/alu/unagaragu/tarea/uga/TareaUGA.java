126187003ad7b927270a8c1cd7ba0ac721831924
Unaig <unagaragu@alu.edu.gva.es>
2024-11-18 19:23:08 +0100

    Tarea Java Maven Closes #1

---
+++ Tarea-UGA/src/main/java/es/gva/edu/alu/unagaragu/tarea/uga/TareaUGA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package es.gva.edu.alu.unagaragu.tarea.uga;
+
+/**
+ *
+ * @author unaig
+ */
+public class TareaUGA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
